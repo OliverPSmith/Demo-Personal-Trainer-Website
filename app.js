413e63abd100a558f944d16fe280294c4b17b249
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2022-03-18 18:44:23 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+/************* Menu Toggle */
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+
+})
